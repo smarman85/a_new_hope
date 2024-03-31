2e4515fe89c0bec6195d81fc654828b9fcf9afe6
Sean <seanmarman@gmail.com>
2024-03-31 16:58:44 -0700

    hidden vars

--- demo/vars.tf
+++ demo/vars.tf
@@ -34,15 +34,3 @@ variable "PUBLIC_KEY_PATH" {
 variable "DNS_NAME" {
   default = "seanmarman.com"
 }
-
-variable "CF_ZONE_ID" {
-  description = "value of the cloudflare zone id"
-}
-
-variable "CF_TOKEN" {
-  description = "value of the cloudflare token"
-}
-
-variable "CF_ACCOUNT_ID" {
-  description = "value of the cloudflare account id"
-}
